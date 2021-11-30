3c687232efff7b777785bb0e8d9bdc3946998b85
Ashish Lattukids <Ashish Gupta>
2021-11-30 21:12:30 +0530

    added task data source interface

---
+++ app/src/main/java/com/example/navigithubpr/data/Source/remote/RemoteDataSource.kt
@@ -0,0 +1,7 @@
+package com.example.navigithubpr.data.Source.remote
+
+import android.app.Application
+import com.example.navigithubpr.data.Source.PrDataSource
+
+class RemoteDataSource(application: Application):PrDataSource {
+}
