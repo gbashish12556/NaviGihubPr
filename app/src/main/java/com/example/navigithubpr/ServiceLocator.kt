1f44f64f408c6a14dd51864a3401a8108cae9e2f
Ashish Lattukids <Ashish Gupta>
2021-11-30 20:54:17 +0530

    added basic code for PrRepository and Result

--- app/src/main/java/com/example/navigithubpr/ServiceLocator.kt
+++ app/src/main/java/com/example/navigithubpr/ServiceLocator.kt
@@ -1,5 +1,5 @@
 package com.example.navigithubpr
 
 class ServiceLocator {
-    
+
 }
--- app/src/main/java/com/example/navigithubpr/data/Source/PrRepository.kt
+++ app/src/main/java/com/example/navigithubpr/data/Source/PrRepository.kt
@@ -1,8 +1,16 @@
 package com.example.navigithubpr.data.Source
 
 import androidx.lifecycle.LiveData
+import com.example.navigithubpr.data.GithubIssuesResponse
 
 interface PrRepository {
 
 
+    fun observeTasks(): LiveData<Result<List<GithubIssuesResponse>>>
+
+    suspend fun getTasks(forceUpdate: Boolean = false): Result<List<GithubIssuesResponse>>
+
+    suspend fun refreshTasks()
+
+    fun observeTask(taskId: String): LiveData<Result<GithubIssuesResponse>>
 }
