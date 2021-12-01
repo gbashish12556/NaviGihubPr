fd58f340924c1f00f690659e48cd13a615a3372c
Ashish Lattukids <Ashish Gupta>
2021-12-01 12:06:12 +0530

    minor code changes

--- app/src/main/java/com/example/navigithubpr/data/Source/DefaultPreRepository.kt
+++ app/src/main/java/com/example/navigithubpr/data/Source/DefaultPreRepository.kt
@@ -1,9 +1,10 @@
 package com.example.navigithubpr.data.Source
 
 import com.example.navigithubpr.data.GithubIssuesResponse
+import com.example.navigithubpr.data.Result
 
 class DefaultPreRepository(private val prDataSource: PrDataSource):PrRepository {
-    override suspend fun getTasks(forceUpdate: Boolean): Result<List<GithubIssuesResponse>> {
-        return prDataSource.getTasks()
+    override suspend fun getTasks(orgName:String, folderName:String): Result<List<GithubIssuesResponse>> {
+        return prDataSource.getTasks(orgName, folderName)
     }
 }
--- app/src/main/java/com/example/navigithubpr/data/Source/PrRepository.kt
+++ app/src/main/java/com/example/navigithubpr/data/Source/PrRepository.kt
@@ -1,10 +1,11 @@
 package com.example.navigithubpr.data.Source
 
 import androidx.lifecycle.LiveData
+import com.example.navigithubpr.data.Result
 import com.example.navigithubpr.data.GithubIssuesResponse
 
 interface PrRepository {
 
-    suspend fun getTasks(forceUpdate: Boolean = false): Result<List<GithubIssuesResponse>>
+    suspend fun getTasks(orgName:String, folderName:String): Result<List<GithubIssuesResponse>>
 
 }
